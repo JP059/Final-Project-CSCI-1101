8f6a7050e9a23c89e8174d1c96d9a5b1e2c9a248
John Reyes <reyes.john1005@gmail.com>
2024-12-02 23:04:20 -0600

    final js

--- Final-Project/js/scripts.js
+++ Final-Project/js/scripts.js
@@ -1,1 +1,17 @@
 // JavaScript source code
+// Function to show the "Back to Top" button when scrolling down
+window.onscroll = function () {
+    var scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        scrollToTopBtn.style.display = "block"; // Show button
+    } else {
+        scrollToTopBtn.style.display = "none"; // Hide button
+    }
+};
+
+// Function to scroll the page back to the top when the button is clicked
+function scrollToTop() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+}
